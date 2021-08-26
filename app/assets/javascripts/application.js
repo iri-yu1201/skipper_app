565913cb5add0251e2363a6c21540baec33e06cd
iriyu-1201 <iri12yuta01@gmail.com>
2021-08-26 09:01:27 +0000

    skippr追加(スライド不完全)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,18 @@ $(document).ready(function(){
        $(this).css('color','red');
    });
 });
+
+$(document).ready(function(){
+   $('#theTarget').skippr({
+      transition : 'slide',
+      speed : 1000,
+      easing : 'easeOutQuart',
+      navType : 'block',
+      childrenElementType : 'div',
+      arrows : true,
+      autoPlay : true,
+      autoPlayDuration : 3000,
+      keyboardOnAlways : true,
+      hidePrevious : false
+   });
+});
